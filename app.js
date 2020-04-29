7c09a2c1d0959990a6ec9835bf26bb28d0133c39
Yeismer Espejo <yespejo@minka.io>
2020-04-28 22:23:59 -0500

    load process

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use ((req, res, next) =>{
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("------------------------------------------------------------------------------------------------( " + ((new Date()).toISOString()) +  " )");
-//var task = cron.schedule("* * * * *", function() {
+var task = cron.schedule("* * * * *", function() {
     console.log("running a task every 1 minute");
 
     const sqs = new AWS.SQS({
@@ -276,7 +276,7 @@ console.log("-------------------------------------------------------------------
             });
       	}
     });
-//});
+});
 
 app.get('/stop', (req,res) => {
     task.stop();
