a5e91a33ebe926c23e36ee031f9128d426321b95
Yeismer Espejo <yespejo@minka.io>
2020-04-28 20:13:26 -0500

    Test load changes repository

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use ((req, res, next) =>{
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("------------------------------------------------------------------------------------------------( " + ((new Date()).toISOString()) +  " )");
-var task = cron.schedule("* * * * *", function() { //se ejecuta cada minuto
+var task = cron.schedule("* * * * *", function() {
     console.log("running a task every 1 minute");
 
     const sqs = new AWS.SQS({
