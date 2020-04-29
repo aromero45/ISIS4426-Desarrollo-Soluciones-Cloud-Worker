129a2d422b4bdfc79783a750beb3f9f82a251b78
Yeismer Espejo <yespejo@minka.io>
2020-04-28 21:57:32 -0500

    Run process each one minute

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use ((req, res, next) =>{
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("------------------------------------------------------------------------------------------------( " + ((new Date()).toISOString()) +  " )");
-//var task = cron.schedule("* * * * *", function() {
+var task = cron.schedule("* * * * *", function() {
     console.log("running a task every 1 minute");
 
     const sqs = new AWS.SQS({
@@ -278,7 +278,7 @@ console.log("-------------------------------------------------------------------
             });
       	}
     });
-//});
+});
 
 app.get('/stop', (req,res) => {
     task.stop();
